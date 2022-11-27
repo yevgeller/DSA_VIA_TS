1de3f081a2773860d73a051cf4ebee0c50421428
yevgeller@gmail.com <yevgeller@gmail.com>
2022-11-26 22:26:53 -0500

    Interim commit

--- 125_Valid_Palindrome/script.js
+++ 125_Valid_Palindrome/script.js
@@ -1,8 +1,12 @@
 function isPalindrome(s) {
-    var regex = /[A-Z]/;
+    if (s.trim().length === 0)
+        return true;
+    var regex = /[A-Z\d]/;
+    //let actualStringFlag: boolean = true;
     var s_char_arr = s.toUpperCase().split('').filter(function (x) { return x.match(regex); }).join('');
+    //console.log(s_char_arr);
     var result = s_char_arr.length > 0 && s_char_arr === s_char_arr.split('').reverse().join('');
-    console.log(s, result);
+    //console.log(s, result);
     return result;
 }
 ;
--- 125_Valid_Palindrome/script.ts
+++ 125_Valid_Palindrome/script.ts
@@ -1,22 +1,15 @@
 function isPalindrome(s: string): boolean {
-    let counterFromBeginningToEnd: number = 0;
-    let counterFromEndToBeginning: number = s.length - 1;
-    let regex = /[A-Z]/;
+    if (s.trim().length === 0) return true;
+    let regex = /[A-Z\d]/;
     //let actualStringFlag: boolean = true;
 
-    for (; counterFromBeginningToEnd < counterFromEndToBeginning; counterFromBeginningToEnd++, counterFromEndToBeginning--) {
-        while (!s[counterFromBeginningToEnd].toUpperCase().match(regex) && counterFromBeginningToEnd < counterFromEndToBeginning) { counterFromBeginningToEnd++; }
-        while (!s[counterFromEndToBeginning].toUpperCase().match(regex) && counterFromEndToBeginning > counterFromBeginningToEnd) { counterFromEndToBeginning--; }
-
-
-        if (s[counterFromBeginningToEnd].toUpperCase() != s[counterFromEndToBeginning].toUpperCase()) {
-            return false;
-        }
-
-        //  actualStringFlag = true;
-    }
-
-    return true; // actualStringFlag && true;
+    let s_char_arr = s.toUpperCase().split('').filter(x => x.match(regex)).join('');
+    //console.log(s_char_arr);
+    let result = s_char_arr.length > 0 && s_char_arr === s_char_arr.split('').reverse().join('');
+    //console.log(s, result);
+    return result;
 };
 
+
+isPalindrome('A man, a plan, a canal: Panama');
 isPalindrome(".,");
