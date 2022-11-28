53127f4768c2955bc5cece407e3c7945321119ff
yevgeller@gmail.com <yevgeller@gmail.com>
2022-11-27 22:39:52 -0500

    Interim commit

--- 125_Valid_Palindrome/script.js
+++ 125_Valid_Palindrome/script.js
@@ -1,16 +1,28 @@
 function isPalindrome(s) {
     var regex = /[A-Z\d]/;
-    var s_char_arr = s.toUpperCase().replace('.', '').replace(',', '').replace('!', '').split('').filter(function (x) { return x.match(regex); }).join('');
-    //console.log(s_char_arr);
+    var s_char_arr = s.toUpperCase().replace('.', '').replace(',', '').replace('!', '').replace(':', '').split('').filter(function (x) { return x.match(regex); }).join('');
     var result = s_char_arr === s_char_arr.split('').reverse().join('');
-    //console.log(s, result);
-    // if (!result && (s.trim().length === 0 ||
-    //     s.trim() === ".," ||
-    //     s.trim() === "!!!"))
-    //     return true; //how are these tests alphanumeric?
     return result;
 }
 ;
-isPalindrome('A man, a plan, a canal: Panama');
+function isPali2(s) {
+    var fromEnd = s.length;
+    var fromBeginning = 0;
+    var regex = /[A-Z\d]/;
+    var i = false;
+    for (; fromEnd > fromBeginning; fromEnd--, fromBeginning++) {
+        while (!s[fromEnd].match(regex) && fromEnd > fromBeginning) {
+            fromEnd--;
+        }
+        while (!s[fromBeginning].match(regex) && fromBeginning < fromEnd) {
+            fromBeginning++;
+        }
+        if (s[fromBeginning].toUpperCase() != s[fromEnd].toUpperCase())
+            return false;
+        i = true;
+    }
+    return i && true;
+}
+isPali2('A man, a plan, a canal: Panama');
 isPalindrome(".,");
 //# sourceMappingURL=script.js.map
--- 125_Valid_Palindrome/script.ts
+++ 125_Valid_Palindrome/script.ts
@@ -1,15 +1,28 @@
 function isPalindrome(s: string): boolean {
     let regex = /[A-Z\d]/;
     let s_char_arr = s.toUpperCase().replace('.', '').replace(',', '').replace('!', '').replace(':', '').split('').filter(x => x.match(regex)).join('');
-    //console.log(s_char_arr);
     let result = s_char_arr === s_char_arr.split('').reverse().join('');
-    //console.log(s, result);
-    // if (!result && (s.trim().length === 0 ||
-    //     s.trim() === ".," ||
-    //     s.trim() === "!!!")) return true; //how are these tests alphanumeric?
     return result;
 };
 
 
-isPalindrome('A man, a plan, a canal: Panama');
+function isPali2(s: string): boolean {
+    let fromEnd = s.length - 1;
+    let fromBeginning = 0;
+    let regex = /[A-Z\d]/;
+    let i = false;
+    for (; fromEnd > fromBeginning; fromEnd--, fromBeginning++) {
+        while (!s[fromEnd].match(regex) && fromEnd > fromBeginning) {
+            fromEnd--;
+        }
+        while (!s[fromBeginning].match(regex) && fromBeginning < fromEnd) {
+            fromBeginning++;
+        }
+        if (s[fromBeginning].toUpperCase() != s[fromEnd].toUpperCase()) return false;
+        i = true;
+    }
+    return i && true;
+}
+
+isPali2('A man, a plan, a canal: Panama');
 isPalindrome(".,");
