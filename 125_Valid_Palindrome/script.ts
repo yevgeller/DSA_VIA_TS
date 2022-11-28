248876f67949e2c146c1931d5d8160e598a94f36
yevgeller@gmail.com <yevgeller@gmail.com>
2022-11-27 22:21:48 -0500

    Interim commit

--- 125_Valid_Palindrome/script.ts
+++ 125_Valid_Palindrome/script.ts
@@ -1,11 +1,10 @@
 function isPalindrome(s: string): boolean {
     let regex = /[A-Z\d]/;
-    let s_char_arr = s.toUpperCase().split('').filter(x => x.match(regex)).join('');
+    let s_char_arr = s.toUpperCase().replace('.', '').split('').filter(x => x.match(regex)).join('');
     //console.log(s_char_arr);
     let result = s_char_arr.length > 0 && s_char_arr === s_char_arr.split('').reverse().join('');
     //console.log(s, result);
     if (!result && (s.trim().length === 0 ||
-        s.trim() === '.' ||
         s.trim() === ".," ||
         s.trim() === "!!!")) return true; //how are these tests alphanumeric?
     return result;
