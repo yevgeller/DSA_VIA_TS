01efaac7489f8f75dd44f934e7ff790ea9e6940f
yevgeller@gmail.com <yevgeller@gmail.com>
2022-11-27 22:27:58 -0500

    Interim commit

--- 125_Valid_Palindrome/script.js
+++ 125_Valid_Palindrome/script.js
@@ -1,12 +1,13 @@
 function isPalindrome(s) {
-    if (s.trim().length === 0)
-        return true;
     var regex = /[A-Z\d]/;
-    //let actualStringFlag: boolean = true;
-    var s_char_arr = s.toUpperCase().split('').filter(function (x) { return x.match(regex); }).join('');
+    var s_char_arr = s.toUpperCase().replace('.', '').replace(',', '').replace('!', '').split('').filter(function (x) { return x.match(regex); }).join('');
     //console.log(s_char_arr);
-    var result = s_char_arr.length > 0 && s_char_arr === s_char_arr.split('').reverse().join('');
+    var result = s_char_arr === s_char_arr.split('').reverse().join('');
     //console.log(s, result);
+    // if (!result && (s.trim().length === 0 ||
+    //     s.trim() === ".," ||
+    //     s.trim() === "!!!"))
+    //     return true; //how are these tests alphanumeric?
     return result;
 }
 ;
--- 125_Valid_Palindrome/script.ts
+++ 125_Valid_Palindrome/script.ts
@@ -2,11 +2,11 @@ function isPalindrome(s: string): boolean {
     let regex = /[A-Z\d]/;
     let s_char_arr = s.toUpperCase().replace('.', '').replace(',', '').replace('!', '').split('').filter(x => x.match(regex)).join('');
     //console.log(s_char_arr);
-    let result = s_char_arr.length > 0 && s_char_arr === s_char_arr.split('').reverse().join('');
+    let result = s_char_arr === s_char_arr.split('').reverse().join('');
     //console.log(s, result);
-    if (!result && (s.trim().length === 0 ||
-        s.trim() === ".," ||
-        s.trim() === "!!!")) return true; //how are these tests alphanumeric?
+    // if (!result && (s.trim().length === 0 ||
+    //     s.trim() === ".," ||
+    //     s.trim() === "!!!")) return true; //how are these tests alphanumeric?
     return result;
 };
 
