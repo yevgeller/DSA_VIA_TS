74070270af1c88a27b686b63c7c95caba1c91248
yevgeller <yevgeller@gmail.com>
2022-11-30 22:59:17 -0500

    724 Interim commit

--- 724_Find_Pivot_Index/script.js
+++ 724_Find_Pivot_Index/script.js
@@ -5,13 +5,16 @@ function pivotIndex(nums) {
     for (var i = 0; i < nums.length; i++) {
         sum += nums[i];
     }
+    console.log("sum", sum);
     var leftSum = nums[0];
-    for (var i = 0; i < nums.length; i++) {
+    for (var i = 1; i < nums.length; i++) {
+        console.log(i, "i", "leftSum", leftSum, "nums[i]", nums[i], "equation", sum - nums[i] - leftSum);
         if (leftSum == sum - nums[i] - leftSum)
             return i;
         leftSum += nums[i];
     }
     return -1;
 }
-console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+//console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+console.log(this.pivotIndex([2, 1, -1]));
 //# sourceMappingURL=script.js.map
--- 724_Find_Pivot_Index/script.ts
+++ 724_Find_Pivot_Index/script.ts
@@ -4,8 +4,19 @@ function pivotIndex(nums: number[]): number {
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
   }
+  console.log("sum", sum);
   let leftSum = nums[0];
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 1; i < nums.length; i++) {
+    console.log(
+      i,
+      "i",
+      "leftSum",
+      leftSum,
+      "nums[i]",
+      nums[i],
+      "equation",
+      sum - nums[i] - leftSum
+    );
     if (leftSum == sum - nums[i] - leftSum) return i;
     leftSum += nums[i];
   }
@@ -13,4 +24,6 @@ function pivotIndex(nums: number[]): number {
   return -1;
 }
 
-console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+//console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+
+console.log(this.pivotIndex([2, 1, -1]));
