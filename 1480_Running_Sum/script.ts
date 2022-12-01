269a54e0428ede2a169c9b79816d1b7f68d38560
yevgeller <yevgeller@gmail.com>
2022-11-30 20:27:35 -0500

    Successful 1480 submission

--- 1480_Running_Sum/script.ts
+++ 1480_Running_Sum/script.ts
@@ -1,1 +1,14 @@
-function runningSum(nums: number[]): number[] {}
+function runningSum(nums: number[]): number[] {
+  if (nums.length === 0) return [];
+  if (nums.length === 1) return nums;
+
+  let result = [nums[0]];
+  let runningTotal: number = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    runningTotal += nums[i];
+    result.push(runningTotal);
+  }
+  return result;
+}
+
+
