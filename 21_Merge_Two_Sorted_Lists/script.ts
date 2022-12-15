f8aae4978a278591e10971b0cd97bf12d64e10fb
yevgeller <yevgeller@gmail.com>
2022-12-14 22:44:16 -0500

    Adding recursive merge two lists solution

--- 21_Merge_Two_Sorted_Lists/script.ts
+++ 21_Merge_Two_Sorted_Lists/script.ts
@@ -10,6 +10,21 @@ class ListNode {
   }
 }
 
+function mergeTwoListsRecursively(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  if (l1 == null) return l2;
+  else if (l2 == null) return l1;
+  else if (l1.val < l2.val) {
+    l1.next = mergeTwoListsRecursively(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoListsRecursively(l2.next, l1);
+    return l2;
+  }
+}
+
 function mergeTwoLists(
   l1: ListNode | null,
   l2: ListNode | null
