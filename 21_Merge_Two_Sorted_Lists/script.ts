ca2b774f854d8b1f294721cb193ecd3aa597a440
yevgeller <yevgeller@gmail.com>
2022-12-14 22:19:33 -0500

    Renaming so that it matches the description

--- 21_Merge_Two_Sorted_Lists/script.ts
+++ 21_Merge_Two_Sorted_Lists/script.ts
@@ -11,25 +11,25 @@ class ListNode {
 }
 
 function mergeTwoLists(
-  list1: ListNode | null,
-  list2: ListNode | null
+  l1: ListNode | null,
+  l2: ListNode | null
 ): ListNode | null {
   let prehead = new ListNode(-1);
 
   let prev = prehead;
 
-  while (list1 != null && list2 != null) {
-    if (list1.val <= list2.val) {
-      prev.next = list1;
-      list1 = list1.next;
+  while (l1 != null && l2 != null) {
+    if (l1.val <= l2.val) {
+      prev.next = l1;
+      l1 = l1.next;
     } else {
-      prev.next = list2;
-      list2 = list2.next;
+      prev.next = l2;
+      l2 = l2.next;
     }
     prev = prev.next;
   }
 
-  prev.next = list1 == null ? list2 : list1;
+  prev.next = l1 == null ? l2 : l1;
 
   return prehead.next;
 }
