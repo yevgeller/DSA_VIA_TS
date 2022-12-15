5e313f0c969fd342c17c48785c9f8700bf901670
yevgeller <yevgeller@gmail.com>
2022-12-14 22:12:33 -0500

    Merge two sorted lists attempt 1

--- 21_Merge_Two_Sorted_Lists/script.ts
+++ 21_Merge_Two_Sorted_Lists/script.ts
@@ -2,17 +2,34 @@
  * Definition for singly-linked list.
  */
 class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  let prehead = new ListNode(-1);
 
-function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  let prev = prehead;
 
+  while (list1 != null && list2 != null) {
+    if (list1.val <= list2.val) {
+      prev.next = list1;
+      list1 = list1.next;
+    } else {
+      prev.next = list2;
+      list2 = list2.next;
+    }
+    prev = prev.next;
+  }
 
-    return null;
-};
+  prev.next = list1 == null ? list2 : list1;
+
+  return prehead.next;
+}
