71449ab2b064d557cf0aa4313ca53a4cc3a01196
yevgeller <yevgeller@gmail.com>
2022-12-15 15:38:24 -0500

    Cleanup 876

--- 876_Middle_Of_Linked_List/script.ts
+++ 876_Middle_Of_Linked_List/script.ts
@@ -1,12 +1,3 @@
-// public ListNode middleNode(ListNode head) {
-//         ListNode slow = head, fast = head;
-//         while (fast != null && fast.next != null) {
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//         return slow;
-//     }
-
 function middleNode(head: ListNode | null): ListNode | null {
   let slow = head;
   let fast = head;
