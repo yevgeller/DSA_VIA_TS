481cf8038de3b272b94f6b7d8b28a2912b67e37d
yevgeller <yevgeller@gmail.com>
2022-12-22 17:31:41 -0500

    142 solution 1

--- 0142_Linked_List_Cycle/script.ts
+++ 0142_Linked_List_Cycle/script.ts
@@ -1,3 +1,14 @@
 function detectCycle(head: ListNode | null): ListNode | null {
+  let visited = new Set<ListNode>();
+  let node = head;
+  while (node != null) {
+    if (visited.has(node)) {
+      return node;
+    }
+
+    visited.add(node);
+    node = node.next;
+  }
+
   return null;
 }
