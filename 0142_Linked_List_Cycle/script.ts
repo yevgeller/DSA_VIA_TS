f6750d1fb0f8274113f617ee69196e28fcf0fdbc
yevgeller <yevgeller@gmail.com>
2022-12-26 22:13:44 -0500

    Tortoise and Hare method

--- 0142_Linked_List_Cycle/script.ts
+++ 0142_Linked_List_Cycle/script.ts
@@ -12,3 +12,35 @@ function detectCycle(head: ListNode | null): ListNode | null {
 
   return null;
 }
+
+//----------- second way, Floyd's Tortoise and Hare
+function getIntersect(head: ListNode | null): ListNode | null {
+  let tortoise = head;
+  let hare = head;
+
+  while (hare != null && hare.next != null) {
+    tortoise = tortoise.next;
+    hare = hare.next.next;
+    if (tortoise == hare) {
+      return tortoise;
+    }
+  }
+
+  return null;
+}
+
+function detectCycle2(head: ListNode | null): ListNode | null {
+  if (head == null) return null;
+
+  let intersect = getIntersect(head);
+
+  let ptr1 = head;
+  let ptr2 = intersect;
+
+  while (ptr1 != ptr2) {
+    ptr1 = ptr1.next;
+    ptr2 = ptr2.next;
+  }
+
+  return ptr1;
+}
