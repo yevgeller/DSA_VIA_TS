e613d49f151bcd122baf1ce810e71319152e58cd
yevgeller <yevgeller@gmail.com>
2022-12-27 20:17:30 -0500

    Interim commit on 121

--- 0121_Best_Time_to_Buy_and_Sell_Stock/script.ts
+++ 0121_Best_Time_to_Buy_and_Sell_Stock/script.ts
@@ -1,1 +1,13 @@
-function maxProfit(prices: number[]): number {}
+function maxProfit(prices: number[]): number {
+  let minPrice: number = number.MAX_SAFE_INTEGER;
+  let maxprofit: number = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    let interim = prices[i] - minPrice;
+
+    if (prices[i] < minPrice) minPrice = prices[i];
+    else if (interim > maxProfit) maxprofit = prices[i] - minPrice;
+  }
+
+  return maxprofit;
+}
