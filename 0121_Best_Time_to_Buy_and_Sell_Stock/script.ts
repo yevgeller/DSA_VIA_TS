5fd60c5b21d6d80f0971e378f4b93712f3526978
yevgeller <yevgeller@gmail.com>
2022-12-27 20:24:20 -0500

    Making it pretty

--- 0121_Best_Time_to_Buy_and_Sell_Stock/script.ts
+++ 0121_Best_Time_to_Buy_and_Sell_Stock/script.ts
@@ -1,10 +1,8 @@
 function maxProfit(prices: number[]): number {
-  let minPrice = 9007199254740991;
+  let minPrice = Number.MAX_SAFE_INTEGER;
   let maxprofit = 0;
 
   for (let i = 0; i < prices.length; i++) {
-    let interim = prices[i] - minPrice;
-
     if (prices[i] < minPrice) minPrice = prices[i];
     else if (prices[i] - minPrice > maxprofit) maxprofit = prices[i] - minPrice;
   }
