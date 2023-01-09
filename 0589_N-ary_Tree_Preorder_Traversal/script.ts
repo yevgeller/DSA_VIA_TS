9f1673258da17b7d44aedb856a3b3a4fb0e7c880
yevgeller <yevgeller@gmail.com>
2023-01-09 17:02:11 -0500

    Update to 589

--- 0589_N-ary_Tree_Preorder_Traversal/script.ts
+++ 0589_N-ary_Tree_Preorder_Traversal/script.ts
@@ -1,15 +1,23 @@
 /**
- * Definition for node.
-  class Node {
-      val: number
-      children: Node[]
-      constructor(val?: number) {
-          this.val = (val===undefined ? 0 : val)
-          this.children = []
-      }
+ * Definition for node.*/
+class Node {
+  val: number;
+  children: Node[];
+  constructor(val?: number) {
+    this.val = val === undefined ? 0 : val;
+    this.children = [];
   }
- 
-*/
+}
+
+//*/
 function preorder(root: Node | null): number[] {
-  return [0];
+  let stack: Array<Node> = [];
+  let output: Array<number> = [];
+  if (root == null) return output;
+  stack.push(root);
+  while (!stack.length) {
+    let node: Node = stack.pop();
+    output.push(node.val);
+  }
+  return output;
 }
