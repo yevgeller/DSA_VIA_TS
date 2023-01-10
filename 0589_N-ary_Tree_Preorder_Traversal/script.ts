9f32ddb0a490adedd54442da5bb0f1cc3f5d3899
yevgeller <yevgeller@gmail.com>
2023-01-10 09:15:29 -0500

    Resolving namespace for 589

--- 0589_N-ary_Tree_Preorder_Traversal/script.ts
+++ 0589_N-ary_Tree_Preorder_Traversal/script.ts
@@ -1,23 +1,25 @@
 /**
  * Definition for node.*/
-class Node {
-  val: number;
-  children: Node[];
-  constructor(val?: number) {
-    this.val = val === undefined ? 0 : val;
-    this.children = [];
+namespace FiveEightyNine {
+  class Node {
+    val: number;
+    children: Node[];
+    constructor(val?: number) {
+      this.val = val === undefined ? 0 : val;
+      this.children = [];
+    }
   }
-}
 
-//*/
-function preorder(root: Node | null): number[] {
-  let stack: Array<Node> = [];
-  let output: Array<number> = [];
-  if (root == null) return output;
-  stack.push(root);
-  while (!stack.length) {
-    let node: Node = stack.pop();
-    output.push(node.val);
+  //*/
+  function preorder(root: Node | null): number[] {
+    let stack: Array<Node> = [];
+    let output: Array<number> = [];
+    if (root == null) return output;
+    stack.push(root);
+    while (!stack.length) {
+      let node: Node = stack.pop();
+      output.push(node.val);
+    }
+    return output;
   }
-  return output;
 }
