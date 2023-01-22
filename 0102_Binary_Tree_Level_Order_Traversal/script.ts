098f52f2ba5150bcc122179d54ee11f542f0e21a
yevgeller <yevgeller@gmail.com>
2023-01-21 21:03:25 -0500

    0102 update

--- 0102_Binary_Tree_Level_Order_Traversal/script.ts
+++ 0102_Binary_Tree_Level_Order_Traversal/script.ts
@@ -1,19 +1,28 @@
-
 //Definition for a binary tree node.
 class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-    }
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
 }
 
-
 function levelOrder(root: TreeNode | null): number[][] {
+  let levels = {};
 
-    let a = [][];
-    return new [][];
+  function bfs(tree: TreeNode | null, level): void {
+    if (!tree) return;
+    if (!levels[level]) levels[level] = [tree.val];
+    else levels[level].push(tree.val);
+
+    if (tree.left) bfs(tree.left, level + 1);
+
+    if (tree.right) bfs(tree.right, level + 1);
+  }
+  bfs(root, 0);
+
+  return Object.values(levels);
 }
