5c0f6bb9cf4c29edeb4d678b4fe58c4a81412686
yevgeller <yevgeller@gmail.com>
2023-01-28 22:21:49 -0500

    binary search

--- 0704_Binary_Search/script.ts
+++ 0704_Binary_Search/script.ts
@@ -1,13 +1,22 @@
 function search(nums: number[], target: number): number {
 let min = 0;
-let max = nums.length / 2;
-look(nums, target, min, max);
+let border = nums.length / 2;
 
-  return -1;
+if(nums[border] == target) return border;
+if(nums[border]<target)
+{
+    return look(nums, target, border, nums.length);
+} else {
+    return look(nums, target, 0, border);
+}
+
+return look(nums, target, min, max);
+
 }
 
 function look(nums: number[], target: number, min: number, max: number): number {
 if(nums[max] == target) return max;
+
 if(nums[max] > target) {
     min = 0; 
     max = max / 2;
