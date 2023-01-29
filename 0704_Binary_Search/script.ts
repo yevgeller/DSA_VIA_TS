eac5349c94d6607ce043958cca6a3837af850e4d
yevgeller <yevgeller@gmail.com>
2023-01-28 22:15:50 -0500

    binary search

--- 0704_Binary_Search/script.ts
+++ 0704_Binary_Search/script.ts
@@ -1,20 +1,20 @@
 function search(nums: number[], target: number): number {
 let min = 0;
 let max = nums.length / 2;
-if(nums[max] == target) return max;
-if(nums[max] > target) {
-    min = 0; 
-    max = max / 2;
-    look(nums, min, max);
-} else {
-    min = max+1;
-    max = 
-}
+look(nums, target, min, max);
 
   return -1;
 }
 
-function look(nums: number[], min: number, max: number): number {
-
+function look(nums: number[], target: number, min: number, max: number): number {
+if(nums[max] == target) return max;
+if(nums[max] > target) {
+    min = 0; 
+    max = max / 2;
+    look(nums, target, min, max);
+} else {
+    min = max+1;
+    max = 
+}
 
 }
