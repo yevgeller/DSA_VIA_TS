a4f914fcdacd6a0e1d989f188ba6188193344aa0
yevgeller <yevgeller@gmail.com>
2023-01-28 22:32:08 -0500

    binary search

--- 0704_Binary_Search/script.ts
+++ 0704_Binary_Search/script.ts
@@ -1,33 +1,42 @@
 function search(nums: number[], target: number): number {
-    return look2(nums, target, 0, nums.length);
+  return look2(nums, target, 0, nums.length);
 
-// let middle = nums.length / 2;
+  // let middle = nums.length / 2;
 
-// if(nums[middle] == target) return middle;
-// if(nums[middle]<target)
-// {
-//     return look(nums, target, middle, nums.length);
+  // if(nums[middle] == target) return middle;
+  // if(nums[middle]<target)
+  // {
+  //     return look(nums, target, middle, nums.length);
+  // } else {
+  //     return look(nums, target, 0, middle);
+  // }
+}
+
+function look2(
+  nums: number[],
+  target: number,
+  min: number,
+  max: number
+): number {
+  let mid = (max - min) / 2;
+
+  if (nums[mid] == target) return mid;
+
+  if (nums[mid] < target) return look2(nums, target, mid + 1, max);
+
+  return look2(nums, target, min, mid - 1);
+}
+
+// function look(nums: number[], target: number, min: number, max: number): number {
+// if(nums[max] == target) return max;
+
+// if(nums[max] > target) {
+//     min = 0;
+//     max = max / 2;
+//     look(nums, target, min, max);
 // } else {
-//     return look(nums, target, 0, middle);
+//     min = max+1;
+//     max =
 // }
 
-
-}
-
-function look2(nums: number[], target: number, min: number, max: number): number {
-
-}
-
-function look(nums: number[], target: number, min: number, max: number): number {
-if(nums[max] == target) return max;
-
-if(nums[max] > target) {
-    min = 0; 
-    max = max / 2;
-    look(nums, target, min, max);
-} else {
-    min = max+1;
-    max = 
-}
-
-}
+// }
