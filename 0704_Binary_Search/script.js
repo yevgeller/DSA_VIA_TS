364c54eb6004782851ecb36be8bc03fa06c4aba0
yevgeller <yevgeller@gmail.com>
2023-01-29 22:06:53 -0500

    704

--- 0704_Binary_Search/script.js
+++ 0704_Binary_Search/script.js
@@ -3,11 +3,15 @@ function search(nums, target) {
 }
 function searchTwo(nums, target, min, max) {
     debugger;
-    var mid = (max - min) / 2;
-    if (nums[mid] == target)
+    var mid = Math.floor((max - min) / 2) + min - 1;
+    if (nums[mid] == target) {
         return mid;
-    if (nums[mid] < target)
+    }
+    else if (nums[mid] < target) {
         return searchTwo(nums, target, mid + 1, max);
-    return searchTwo(nums, target, min, mid - 1);
+    }
+    else {
+        return searchTwo(nums, target, min, mid - 1);
+    }
 }
 search([-1, 0, 3, 5, 9, 12], 9);
