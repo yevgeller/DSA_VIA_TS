e2896a4a08219cbb8aafd4940bdaa593644c6559
yevgeller <yevgeller@gmail.com>
2023-01-29 22:06:48 -0500

    704

--- 0704_Binary_Search/script.ts
+++ 0704_Binary_Search/script.ts
@@ -9,13 +9,15 @@ function searchTwo(
   max: number
 ): number {
   debugger;
-  let mid = (max - min) / 2;
+  let mid = Math.floor((max - min) / 2) + min - 1;
 
-  if (nums[mid] == target) return mid;
-
-  if (nums[mid] < target) return searchTwo(nums, target, mid + 1, max);
-
-  return searchTwo(nums, target, min, mid - 1);
+  if (nums[mid] == target) {
+    return mid;
+  } else if (nums[mid] < target) {
+    return searchTwo(nums, target, mid + 1, max);
+  } else {
+    return searchTwo(nums, target, min, mid - 1);
+  }
 }
 
 search([-1, 0, 3, 5, 9, 12], 9);
