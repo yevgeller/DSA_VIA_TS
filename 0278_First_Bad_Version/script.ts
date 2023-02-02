a678461abe0dba4157e5207ad5ea2be523776a83
yevgeller <yevgeller@gmail.com>
2023-02-02 16:32:52 -0500

    278

---
+++ 0278_First_Bad_Version/script.ts
@@ -0,0 +1,12 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function (isBadVersion: any) {
+  return function (n: number): number {
+    return -1;
+  };
+};
