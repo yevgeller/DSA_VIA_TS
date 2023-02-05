2b6c3e4b1b090fe9fae643ad27caa60006b780f4
yevgeller <yevgeller@gmail.com>
2023-02-05 18:42:00 -0500

    278

--- 0278_First_Bad_Version/script.ts
+++ 0278_First_Bad_Version/script.ts
@@ -7,6 +7,15 @@
 
 var solution = function (isBadVersion: any) {
   return function (n: number): number {
+    let min = 0,
+      max = n;
+    while (min <= max) {
+      let mid = (max - min) / 2 + min;
+      if(isBadVersion(mid)){
+        //bad version, need to move on
+      }
+    }
+
     return -1;
   };
 };
