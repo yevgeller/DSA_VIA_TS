a4fa715be033b6d97508f616789b3e7af5dcc64c
yevgeller <yevgeller@gmail.com>
2023-02-12 16:50:41 -0500

    235

--- 0235_Lowest_Common_Ancestor_of_a_BST/script.ts
+++ 0235_Lowest_Common_Ancestor_of_a_BST/script.ts
@@ -1,26 +1,26 @@
-// //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/solutions/2508999/typescript-javascript-dfs-with-comments/
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/solutions/2508999/typescript-javascript-dfs-with-comments/
 
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  return dfs(root, p, q);
+}
 
-// function lowestCommonAncestor(
-//   root: TreeNode | null,
-//   p: TreeNode | null,
-//   q: TreeNode | null
-// ): TreeNode | null {return dfs(root, p, q);}
+function dfs(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null) {
+  // if any of the values are null, just return the value of root
+  if ([root, p, q].includes(null)) return root;
 
+  // if both values are greater than root
+  // the LCS is in right subtree
+  if (p.val > root.val && q.val > root.val) return dfs(root.right, p, q);
 
-// function dfs(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null) {
-//   // if any of the values are null, just return the value of root
-//   if ([root, p, q].includes(null)) return root;
+  // if both values are less than root
+  // then LCS is in left subtree
+  if (p.val < root.val && q.val < root.val) return dfs(root.left, p, q);
 
-//   // if both values are greater than root
-//   // the LCS is in right subtree
-//   if (p.val > root.val && q.val > root.val) return dfs(root.right, p, q);
-
-//   // if both values are less than root
-//   // then LCS is in left subtree
-//   if (p.val < root.val && q.val < root.val) return dfs(root.left, p, q);
-
-//   // if there's a split
-//   // then we found our LCS
-//   return root;
-// }
+  // if there's a split
+  // then we found our LCS
+  return root;
+}
