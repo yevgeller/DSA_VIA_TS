303cf935789924453f05ec7779bb317e6d9d5a7f
yevgeller <yevgeller@gmail.com>
2023-02-14 08:23:24 -0500

    733

--- 0733_Flood_Fill/script.ts
+++ 0733_Flood_Fill/script.ts
@@ -4,44 +4,44 @@ function floodFill(
   sc: number,
   color: number
 ): number[][] {
-  const queue = new Queue();
+  //   const queue = new Queue();
 
-  queue.enqueue([sr, sc]);
+  //   queue.enqueue([sr, sc]);
 
-  const curCol = image[sr][sc];
+  //   const curCol = image[sr][sc];
 
-  const rowList = [1, 0, 0, -1];
-  const colList = [0, 1, -1, 0];
+  //   const rowList = [1, 0, 0, -1];
+  //   const colList = [0, 1, -1, 0];
 
-  const rowLen = image.length;
-  const colLen = image[0].length;
+  //   const rowLen = image.length;
+  //   const colLen = image[0].length;
 
-  image[sr][sc] = color;
+  //   image[sr][sc] = color;
 
-  if (color === curCol) return image;
+  //   if (color === curCol) return image;
 
-  while (!queue.isEmpty()) {
-    const [row, col] = queue.dequeue();
+  //   while (!queue.isEmpty()) {
+  //     const [row, col] = queue.dequeue();
 
-    for (let i = 0; i < 4; i++) {
-      const deltaRow = row + rowList[i];
-      const deltaCol = col + colList[i];
+  //     for (let i = 0; i < 4; i++) {
+  //       const deltaRow = row + rowList[i];
+  //       const deltaCol = col + colList[i];
 
-      if (
-        deltaRow < 0 ||
-        deltaCol < 0 ||
-        deltaRow >= rowLen ||
-        deltaCol >= colLen
-      )
-        continue;
+  //       if (
+  //         deltaRow < 0 ||
+  //         deltaCol < 0 ||
+  //         deltaRow >= rowLen ||
+  //         deltaCol >= colLen
+  //       )
+  //         continue;
 
-      if (deltaRow === row && deltaCol === col) continue;
-      if (image[deltaRow][deltaCol] === curCol) {
-        image[deltaRow][deltaCol] = color;
-        queue.enqueue([deltaRow, deltaCol]);
-      }
-    }
-  }
+  //       if (deltaRow === row && deltaCol === col) continue;
+  //       if (image[deltaRow][deltaCol] === curCol) {
+  //         image[deltaRow][deltaCol] = color;
+  //         queue.enqueue([deltaRow, deltaCol]);
+  //       }
+  //     }
+  //   }
 
   return image;
 }
